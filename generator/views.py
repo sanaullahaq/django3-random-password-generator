453ea1b41ca8c4904fcec50e70b5253ee6be263a
sanaullahaq <sanaullahaq1997@gmail.com>
2020-07-23 08:17:03 +0600

    our first commit

---
+++ generator/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'generator/home.html')
+
+
+def password(request):
+    character = list('abcdefghijklmnopqrstuvwxyz') # list will split the whole string into a single single character
+    
+    if request.GET.get('uppercase'):
+        character.extend(list('ABCDEFGHIJKLMNOPQRSTVWXYZ'))
+
+    if request.GET.get('number'):
+        character.extend('1234567890')
+
+    if request.GET.get('special'):
+        character.extend('!@#$%^&*')
+
+    length = int(request.GET.get('length', 12)) # just giving a initial value 12 if user does not provides any length
+    the_password = ''
+    for i in range(length):
+        the_password += random.choice(character)
+
+    return render(request, 'generator/password.html', {'password': the_password})
+
+
+def about(request):
+    return render(request, 'generator/about.html')
+
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\django_udemy_nick-walter\password_generator\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
