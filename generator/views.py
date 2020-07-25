56410266a2f9b790e25472aa10496e139512eece
sanaullahaq <sanaullahaq1997@gmail.com>
2020-07-25 06:51:12 +0600

    special character in password added

--- generator/views.py
+++ generator/views.py
@@ -11,8 +11,8 @@ def home(request):
 
 
 def password(request):
-    character = list('abcdefghijklmnopqrstuvwxyz') # list will split the whole string into a single single character
-    
+    character = list('abcdefghijklmnopqrstuvwxyz')  # list will split the whole string into a single single character
+
     if request.GET.get('uppercase'):
         character.extend(list('ABCDEFGHIJKLMNOPQRSTVWXYZ'))
 
@@ -22,14 +22,24 @@ def password(request):
     if request.GET.get('special'):
         character.extend('!@#$%^&*')
 
-    length = int(request.GET.get('length', 12)) # just giving a initial value 12 if user does not provides any length
+    c = ''
+    if request.GET.get('specialChar'):
+        c = request.GET.get('specialChar')
+
+    length = int(request.GET.get('length', 12))  # just giving a initial value 12 if user does not provides any length
     the_password = ''
-    for i in range(length):
-        the_password += random.choice(character)
+
+    if request.GET.get('specialCharatEnd'):
+        for i in range(length):
+            the_password += random.choice(character)
+        the_password += c
+    else:
+        the_password = c
+        for i in range(length):
+            the_password += random.choice(character)
 
     return render(request, 'generator/password.html', {'password': the_password})
 
 
 def about(request):
     return render(request, 'generator/about.html')
-
